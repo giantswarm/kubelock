f63479ad002659b50b723756a15ccd037846b039
Ferran Tomas <ferrandinand@users.noreply.github.com>
2019-08-08 16:06:27 +0200

    Added testing setup

---
+++ integration/setup/setup.go
@@ -0,0 +1,38 @@
+// +build k8srequired
+
+package setup
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/giantswarm/microerror"
+)
+
+func Setup(m *testing.M, config Config) {
+	ctx := context.Background()
+
+	v, err := setup(ctx, m, config)
+	if err != nil {
+		config.Logger.LogCtx(ctx, "level", "error", "message", "failed to setup test environment", "stack", fmt.Sprintf("%#v", err))
+		os.Exit(1)
+	}
+
+	os.Exit(v)
+}
+
+func setup(ctx context.Context, m *testing.M, config Config) (int, error) {
+
+	// Create namespace.
+	{
+		err := config.K8sSetup.EnsureNamespaceCreated(ctx, "testing")
+		if err != nil {
+			return 0, microerror.Mask(err)
+		}
+	}
+
+	v := m.Run()
+	return v, nil
+}
