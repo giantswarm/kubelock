f2052ca230fb54175bd9a9aa3efffb51a0fed707
Paweł Kopiczko <kopiczko@gmail.com>
2019-08-09 16:23:05 +0100

    alphabet

--- key.go
+++ key.go
@@ -4,18 +4,18 @@ import (
 	"time"
 )
 
-func isExpired(data lockData) bool {
-	return data.CreatedAt.Add(data.TTL).Before(time.Now())
-}
-
-func lockAnnotation(name string) string {
-	return "kubelock.giantswarm.io/" + name
-}
-
 func defaultedOptions(options LockOptions) LockOptions {
 	if options.TTL == 0 {
 		options.TTL = DefaultTTL
 	}
 
 	return options
+}
+
+func isExpired(data lockData) bool {
+	return data.CreatedAt.Add(data.TTL).Before(time.Now())
+}
+
+func lockAnnotation(name string) string {
+	return "kubelock.giantswarm.io/" + name
 }
