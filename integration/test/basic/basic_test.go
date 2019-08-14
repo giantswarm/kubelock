8f58d357f7e1cb768afad220d438a2b357ad8cf4
Paweł Kopiczko <kopiczko@gmail.com>
2019-08-14 14:13:16 +0100

    test/basic: add basic acquire/release test
    Towards https://github.com/giantswarm/giantswarm/issues/6691.

--- integration/test/basic/basic_test.go
+++ integration/test/basic/basic_test.go
@@ -3,9 +3,56 @@
 package basic
 
 import (
+	"context"
 	"testing"
+
+	"github.com/giantswarm/kubelock"
+
+	"github.com/giantswarm/microerror"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 )
 
 func TestBasic(t *testing.T) {
-	t.Log("Basic test")
+	var err error
+	ctx := context.Background()
+
+	var kubeLock *kubelock.KubeLock
+	{
+		c := kubelock.Config{
+			DynClient: config.K8sClients.DynClient(),
+
+			GVR: schema.GroupVersionResource{
+				Group:    "core",
+				Version:  "v1",
+				Resource: "namespaces",
+			},
+		}
+
+		kubeLock, err = kubelock.New(c)
+		if err != nil {
+			t.Fatalf("error == %#q, want nil", microerror.Stack(err))
+		}
+	}
+
+	lock := kubeLock.Lock("test-lock")
+
+	err = lock.Acquire(ctx, "default")
+	if err != nil {
+		t.Fatalf("error == %#q, want nil", microerror.Stack(err))
+	}
+
+	err = lock.Acquire(ctx, "default")
+	if !kubelock.IsAlreadyExist(err) {
+		t.Fatalf("error == %#v, want matching kubelock.IsAlreadyExist", err)
+	}
+
+	err = lock.Release(ctx, "default")
+	if err != nil {
+		t.Fatalf("error == %#q, want nil", microerror.Stack(err))
+	}
+
+	err = lock.Release(ctx, "default")
+	if !kubelock.IsNotFound(err) {
+		t.Fatalf("error == %#v, want matching kubelock.IsNotFound", err)
+	}
 }
