7538b7b053237ff7fb4e9925f3eed85c95c31733
Jihyuk Bok <jhbok@giantswarm.io>
2020-07-15 10:28:21 +0200

    apply k8s 1.18.5

--- integration/setup/config.go
+++ integration/setup/config.go
@@ -3,15 +3,11 @@
 package setup
 
 import (
-	"github.com/giantswarm/k8sclient"
+	"github.com/giantswarm/k8sclient/v3/pkg/k8sclient"
 	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/micrologger"
 
 	"github.com/giantswarm/kubelock/integration/env"
-)
-
-const (
-	namespace = "giantswarm"
 )
 
 type Config struct {
